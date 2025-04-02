301b7c15a2dd450d9c554d62c049b7dc554ad8d3
malagar-amm <tepid-wig0v@icloud.com>
2025-04-02 11:15:54 +0800

    Added polynomial structure

--- mp01.c
+++ mp01.c
@@ -1,2 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
+
+typedef struct node {
+    int exp_x;
+    int exp_y;
+    int exp_z;
+    int coeff;
+    struct node* next;
+
+} Node;
+
+typedef struct polynomial {
+    int id_num; // the identifier of the polynomial
+    Node* head;
+
+} Polynomial;
+
+Node* createNode() {
+    Node* new_node=(Node*)malloc(sizeof(Node));
+
+    new_node->exp_x = 0;
+    new_node->exp_y = 0;
+    new_node->exp_z = 0;
+    new_node->coeff = 0;
+    new_node->next = NULL;
+
+    return new_node;
+}
